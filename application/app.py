16762b4cc6ef027c808dad4118c68a54de9ead68
RohanBruce-M <113763246+RohanBruce-M@users.noreply.github.com>
2022-09-28 16:19:32 +0530

    first commit

--- application/app.py
+++ application/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST','GET'])
+@app.route('/', methods = ['POST','GET'])
 
 def calculate():
     bmi = ''
